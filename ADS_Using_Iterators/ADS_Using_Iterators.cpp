3a7337376df1fb9b68a83c07f79b866a127be0fd
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-22 09:44:51 +0100

    added 4 print

--- ADS_Using_Iterators/ADS_Using_Iterators.cpp
+++ ADS_Using_Iterators/ADS_Using_Iterators.cpp
@@ -17,17 +17,29 @@ void demoUsingListIterators()
 {
 	list<int> intList = { 7,8,9,10 };
 
+#pragma region Loop demos
+
+	//loop and print
 	for (int x : intList)
 		cout << x << endl;
 
+	//loop and print
 	for (auto x : intList)
 		cout << x << endl;
 
+	//loop and print
 	list<int>::iterator iter = intList.begin();
-
 	while (iter != intList.end())
 	{
 		cout << *iter << endl;
 		iter++;
 	}
+
+	//loop and print
+	auto it = intList.end();
+	while (it != intList.begin()) {
+		it--;
+		cout << *it << endl;
+	}
+#pragma endregion
 }
