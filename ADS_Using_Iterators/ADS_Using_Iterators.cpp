e8dd5136f5675209861aeaef854d8dc1a58aa5c7
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-22 09:20:34 +0100

    Add project files.

---
+++ ADS_Using_Iterators/ADS_Using_Iterators.cpp
@@ -0,0 +1,16 @@
+// ADS_Using_Iterators.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "ADS_Using_Iterators.h"
+
+void demoUsingListIterators();
+
+int main()
+{
+	demoUsingListIterators();
+}
+
+void demoUsingListIterators()
+{
+}
