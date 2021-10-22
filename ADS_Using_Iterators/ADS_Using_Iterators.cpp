0b08b0f0f9ef21b401f3382f83e55ebbd58f4cfd
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-22 09:22:11 +0100

    Initial commit

--- ADS_Using_Iterators/ADS_Using_Iterators.cpp
+++ ADS_Using_Iterators/ADS_Using_Iterators.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include "ADS_Using_Iterators.h"
+#include <list>
+using namespace std;
 
 void demoUsingListIterators();
 
@@ -13,4 +15,5 @@ int main()
 
 void demoUsingListIterators()
 {
+	list<int> intList = { 1,2,3,4,5,6,7,8,9,10 };
 }
